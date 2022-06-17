20f60b53d815ca5a2ae5d73c5b542676cbc86a97
manuel <manuel.cargnel@rwth-aachen.de>
2022-06-17 17:51:21 +0200

    bugfixes and mqtt improvements

--- logger/modbus-huawei-tcp-rtu.py
+++ logger/modbus-huawei-tcp-rtu.py
@@ -14,97 +14,142 @@ client = ModbusClient("192.168.21.150", port=502, framer=ModbusRtuFramer)
 mqtt_client =mqtt.Client("huawei")
 mqtt_client.connect("192.168.21.23", port=1883, keepalive=60, bind_address="")
 
+yield_today_=yield_total_=today_peak_power_=active_power_=0
+ac_c1_=ac_c2_=ac_c3_=0
+pv_v1_=pv_v2_=pv_v3_=pv_v4_=0
+pv_c1_=pv_c2_=pv_c3_=pv_c4_=0
+p1_=p2_=p3_=p4_=0
+ac_p1_=ac_p2_=ac_p3_=0
+ac_v1_=ac_v2_=ac_v3_=0
+pf_=pf2_=0
+
 for x in range(20000):
     
     #if hasattr(response, "registers"):
         
     response = client.read_holding_registers(address=32114, count=2, unit=1)
-    yield_today = np.left_shift(np.int32(response.registers[0]),16) | np.int32(response.registers[1])
-    mqtt_client.publish("huawei/yield_today", payload=float(yield_today)/100.0)
+    yield_today = np.left_shift(np.int32(response.registers[0]),16) | np.int32(response.registers[1])/100.0
+    if(yield_today != yield_today_):
+        mqtt_client.publish("huawei/yield_today", payload=yield_today)
         
     response = client.read_holding_registers(address=32106, count=2, unit=1)
-    yield_total = np.left_shift(np.int32(response.registers[0]),16) | np.int32(response.registers[1])
-    mqtt_client.publish("huawei/yield_total", payload=float(yield_total)/100.0)
+    yield_total = np.left_shift(np.int32(response.registers[0]),16) | np.int32(response.registers[1])/100.0
+    if(p1 != p1_):
+        mqtt_client.publish("huawei/yield_total", payload=yield_total
         
     response = client.read_holding_registers(address=32078, count=2, unit=1)
-    today_peak_power = np.int32(np.left_shift(np.int16(response.registers[0]),16) | np.int32(response.registers[1]))
-    mqtt_client.publish("huawei/today_peak_power", payload=float(today_peak_power)/1000.0)
+    today_peak_power = np.int32(np.left_shift(np.int16(response.registers[0]),16) | np.int32(response.registers[1]))/1000.0
+    if(today_peak_power != today_peak_power_):
+        mqtt_client.publish("huawei/today_peak_power", payload=today_peak_power
     
     response = client.read_holding_registers(address=32080, count=2, unit=1)
     active_power = np.int32(np.left_shift(np.int16(response.registers[0]),16) | np.int32(response.registers[1]))
-    mqtt_client.publish("huawei/active_power", payload=float(active_power)/1000.0)
+    if(active_power != active_power_):
+        mqtt_client.publish("huawei/active_power", payload=float(active_power)/1000.0)
 
     response = client.read_holding_registers(address=32072, count=2, unit=1)
     ac_c1 = float(np.int32(np.left_shift(np.int16(response.registers[0]),16) | np.int32(response.registers[1])))/1000.0
-    mqtt_client.publish("huawei/ac_c1", payload=ac_c1)
+    if(ac_c1 != ac_c1_):
+        mqtt_client.publish("huawei/ac_c1", payload=ac_c1)
     
     response = client.read_holding_registers(address=32074, count=2, unit=1)
     ac_c2 = float(np.int32(np.left_shift(np.int16(response.registers[0]),16) | np.int32(response.registers[1])))/1000.0
-    mqtt_client.publish("huawei/ac_c2", payload=ac_c2)
+    if(ac_c2 != ac_c2_):
+        mqtt_client.publish("huawei/ac_c2", payload=ac_c2)
     
     response = client.read_holding_registers(address=32076, count=2, unit=1)
     ac_c3 = float(np.int32(np.left_shift(np.int16(response.registers[0]),16) | np.int32(response.registers[1])))/1000.0
-    mqtt_client.publish("huawei/ac_c3", payload=ac_c3)
+    if(ac_c3 != ac_c3_):
+        mqtt_client.publish("huawei/ac_c3", payload=ac_c3)
     
     response = client.read_holding_registers(address=32016, count=1, unit=1)
-    pv_v1 = np.int16(response.registers[0])
-    mqtt_client.publish("huawei/pv_v1", payload=float(pv_v1)/10.0)
+    pv_v1 = np.int16(response.registers[0])/10.0
+    if(pv_v1 != pv_v1_):
+        mqtt_client.publish("huawei/pv_v1", payload=pv_v1)
     
     response = client.read_holding_registers(address=32018, count=1, unit=1)
-    pv_v2 = np.int16(response.registers[0])
-    mqtt_client.publish("huawei/pv_v2", payload=float(pv_v2)/10.0)
+    pv_v2 = np.int16(response.registers[0])/10.0
+    if(pv_v2 != pv_v2_):
+        mqtt_client.publish("huawei/pv_v2", payload=pv_v2)
     
     response = client.read_holding_registers(address=32020, count=1, unit=1)
-    pv_v3 = np.int16(response.registers[0])
-    mqtt_client.publish("huawei/pv_v3", payload=float(pv_v3)/10.0)
+    pv_v3 = np.int16(response.registers[0])/10.0
+    if(pv_v3 != pv_v3_):
+        mqtt_client.publish("huawei/pv_v3", payload=pv_v3)
     
     response = client.read_holding_registers(address=32022, count=1, unit=1)
-    pv_v4 = np.int16(response.registers[0])
-    mqtt_client.publish("huawei/pv_v4", payload=float(pv_v4)/10.0)
-    
+    pv_v4 = np.int16(response.registers[0])/10.0
+    if(pv_v4 != pv_v4_):
+        mqtt_client.publish("huawei/pv_v4", payload=pv_v4)
+                        
     response = client.read_holding_registers(address=32017, count=1, unit=1)
     pv_c1 = float(np.int16(response.registers[0]))/100.0
-    mqtt_client.publish("huawei/pv_c1", payload=pv_c1)
+    if(pv_c1 != pv_c1_):
+        mqtt_client.publish("huawei/pv_c1", payload=pv_c1)
     
     response = client.read_holding_registers(address=32019, count=1, unit=1)
     pv_c2 = float(np.int16(response.registers[0]))/100.0
-    mqtt_client.publish("huawei/pv_c2", payload=pv_c2)
+    if(pv_c2 != pv_c2_):
+        mqtt_client.publish("huawei/pv_c2", payload=pv_c2)
     
     response = client.read_holding_registers(address=32021, count=1, unit=1)
     pv_c3 = float(np.int16(response.registers[0]))/100.0
-    mqtt_client.publish("huawei/pv_c3", payload=pv_c3)
+    if(pv_c3 != pv_c3_):    
+        mqtt_client.publish("huawei/pv_c3", payload=pv_c3)
     
     response = client.read_holding_registers(address=32023, count=1, unit=1)
-    pv_c4 = np.int16(response.registers[0])
-    mqtt_client.publish("huawei/pv_c4", payload=float(pv_c4)/100.0)
+    pv_c4 = np.int16(response.registers[0])/100.0
+    if(pv_c4 != pv_c4_):
+        mqtt_client.publish("huawei/pv_c4", payload=pv_c4)
     
-    mqtt_client.publish("huawei/pv_p1", payload=(float(pv_c1)/100.0*float(pv_v1)/10.0/1000.0))
-    mqtt_client.publish("huawei/pv_p2", payload=(float(pv_c2)/100.0*float(pv_v2)/10.0/1000.0))
-    mqtt_client.publish("huawei/pv_p3", payload=(float(pv_c3)/100.0*float(pv_v3)/10.0/1000.0))
-    mqtt_client.publish("huawei/pv_p4", payload=(float(pv_c4)/100.0*float(pv_v4)/10.0/1000.0))
+    p1= pv_c1*pv_v1
+    p2= pv_c2*pv_v2
+    p3= pv_c3*pv_v3
+    p4= pv_c4*pv_v4
+    
+    if(p1 != p1_):
+        mqtt_client.publish("huawei/pv_p1", payload=p1)
+    if(p2 != p2_):
+        mqtt_client.publish("huawei/pv_p2", payload=p2)
+    if(p3 != p3_):
+        mqtt_client.publish("huawei/pv_p3", payload=p3)
+    if(p4 != p4_):
+        mqtt_client.publish("huawei/pv_p4", payload=p4)
       
     response = client.read_holding_registers(address=32069, count=1, unit=1)
     ac_v1 = float(np.int16(response.registers[0]))/10.0
-    mqtt_client.publish("huawei/ac_v1", payload=ac_v1)
+    if(ac_v1 != ac_v1_):
+        mqtt_client.publish("huawei/ac_v1", payload=ac_v1)
     
     response = client.read_holding_registers(address=32070, count=1, unit=1)
     ac_v2 = float(np.int16(response.registers[0]))/10.0
-    mqtt_client.publish("huawei/ac_v2", payload=ac_v2)
+    if(ac_v2 != ac_v2_):
+        mqtt_client.publish("huawei/ac_v2", payload=ac_v2)
     
     response = client.read_holding_registers(address=32071, count=1, unit=1)
     ac_v3 = float(np.int16(response.registers[0]))/10.0
-    mqtt_client.publish("huawei/ac_v3", payload=ac_v3)
+    if(ac_v3 != ac_v3_):
+        mqtt_client.publish("huawei/ac_v3", payload=ac_v3)
     
     response = client.read_holding_registers(address=32084, count=1, unit=1)
     pf = float(np.int16(response.registers[0]))/1000.0
-    mqtt_client.publish("huawei/pf", payload=pf)
+    if(pf != pf_):
+        mqtt_client.publish("huawei/pf", payload=pf)
     
     response = client.read_holding_registers(address=40122, count=1, unit=1)
     pf2 = float(np.int16(response.registers[0]))/1000.0
-    mqtt_client.publish("huawei/pf2", payload=pf)
+    if(pf2 != pf2_):
+        mqtt_client.publish("huawei/pf2", payload=pf2)
     
-    mqtt_client.publish("huawei/ac_p1", payload=(ac_v1*ac_c1*pf))
-    mqtt_client.publish("huawei/ac_p2", payload=(ac_v2*ac_c2*pf))
-    mqtt_client.publish("huawei/ac_p3", payload=(ac_v3*ac_c3*pf))
+    ac_p1 = ac_v1*ac_c1*pf
+    ac_p2 = ac_v2*ac_c2*pf
+    ac_p3 = ac_v3*ac_c3*pf
+    
+    if(ac_p1 != ac_p1_):
+        mqtt_client.publish("huawei/ac_p1", payload=ac_p1)
+    if(ac_p2 != ac_p2_):
+        mqtt_client.publish("huawei/ac_p2", payload=ac_p2)
+    if(ac_p3 != ac_p3_):
+        mqtt_client.publish("huawei/ac_p3", payload=ac_p3)
 
 client.close()
--- logger/modbus-tcp.py
+++ logger/modbus-tcp.py
@@ -13,6 +13,15 @@ client = ModbusClient("192.168.21.147", port=502, framer=ModbusRtuFramer)
 mqtt_client =mqtt.Client("bossMQTT")
 mqtt_client.connect("192.168.21.23", port=1883, keepalive=60, bind_address="")
 
+p1_= p2_= p3_= psum_ = 0
+v1_= v2_= v3_ = 0
+c1_= c2_= c3_ = 0
+pf_= pf1_= pf2_= pf3_ = 0
+ee_= ef_ = 0
+ef1_= ef2_= ef3_ = 0
+ee1_= ee2_= ee3_ = 0
+f_ = 0
+
 for x in range(20000):
     
     response = client.read_holding_registers(address=0x5B00, count=0x3E, unit=1)
@@ -20,73 +29,102 @@ for x in range(20000):
     if hasattr(response, "registers"):
         # -----------
         #power
-        p1 = np.int32(np.left_shift(np.int16(response.registers[0x16]),16) | np.int32(response.registers[0x17])) / np.int32(100)
-        p2 = np.int32(np.left_shift(np.int16(response.registers[0x18]),16) | np.int32(response.registers[0x19])) / np.int32(100)
-        p3 = np.int32(np.left_shift(np.int16(response.registers[0x1A]),16) | np.int32(response.registers[0x1B])) / np.int32(100)
-        psum = np.int32(np.left_shift(np.int16(response.registers[0x14]),16) | np.int32(response.registers[0x15])) / np.int32(100)
+        p1 = np.int32(np.left_shift(np.int16(response.registers[0x16]),16) | np.int32(response.registers[0x17])) / 100.0
+        p2 = np.int32(np.left_shift(np.int16(response.registers[0x18]),16) | np.int32(response.registers[0x19])) / 100.0
+        p3 = np.int32(np.left_shift(np.int16(response.registers[0x1A]),16) | np.int32(response.registers[0x1B])) / 100.0
+        psum = np.int32(np.left_shift(np.int16(response.registers[0x14]),16) | np.int32(response.registers[0x15])) / 100.0
             
-        mqtt_client.publish("smartmeter/p1", payload=p1)
-        mqtt_client.publish("smartmeter/p2", payload=p2)
-        mqtt_client.publish("smartmeter/p3", payload=p3)
-        mqtt_client.publish("smartmeter/psum", payload=(psum))
+        if(p1 != p1_):
+            mqtt_client.publish("smartmeter/p1", payload=p1)
+        if(p2 != p2_):
+            mqtt_client.publish("smartmeter/p2", payload=p2)
+        if(p3 != p3_):
+            mqtt_client.publish("smartmeter/p3", payload=p3)
+        if(psum != psum_):
+            mqtt_client.publish("smartmeter/psum", payload=psum)
 
         # # -----------
         # # voltages
-        v1 = ((response.registers[0x0] << 16) | response.registers[0x1]) / 10
-        v2 = ((response.registers[0x2] << 16) | response.registers[0x3]) / 10
-        v3 = ((response.registers[0x4] << 16) | response.registers[0x5]) / 10
 
-        mqtt_client.publish("smartmeter/v1", payload=v1)
-        mqtt_client.publish("smartmeter/v2", payload=v2)
-        mqtt_client.publish("smartmeter/v3", payload=v3)
+        v1 = ((response.registers[0x0] << 16) | response.registers[0x1]) / 10.0
+        v2 = ((response.registers[0x2] << 16) | response.registers[0x3]) / 10.0
+        v3 = ((response.registers[0x4] << 16) | response.registers[0x5]) / 10.0
+
+        if(v1 != v1_):
+            mqtt_client.publish("smartmeter/v1", payload=v1)
+        if(v2 != v2_):
+            mqtt_client.publish("smartmeter/v2", payload=v2)
+        if(v3 != v3_):
+            mqtt_client.publish("smartmeter/v3", payload=v3)
 
         #Phase current
         c1 = ((response.registers[0x0C] << 16) | response.registers[0x0D]) / 100.0 * np.sign(p1)
         c2 = ((response.registers[0x0E] << 16) | response.registers[0x0F]) / 100.0 * np.sign(p2)
         c3 = ((response.registers[0x10] << 16) | response.registers[0x11]) / 100.0 * np.sign(p3)
         
-        mqtt_client.publish("smartmeter/c1", payload=c1)
-        mqtt_client.publish("smartmeter/c2", payload=c2)
-        mqtt_client.publish("smartmeter/c3", payload=c3)
+        if(c1 != c1_):
+            mqtt_client.publish("smartmeter/c1", payload=c1)
+        if(c2 != c2_):
+            mqtt_client.publish("smartmeter/c2", payload=c2)
+        if(c3 != c3_):
+            mqtt_client.publish("smartmeter/c3", payload=c3)
         
         # power factor
-        pf = np.int16(response.registers[0x3A])/1000
-        pf1 = np.int16(response.registers[0x3B])/1000
-        pf2 = np.int16(response.registers[0x3C])/1000
-        pf3 = np.int16(response.registers[0x3D])/1000
+        pf = np.int16(response.registers[0x3A])/1000.0
+        pf1 = np.int16(response.registers[0x3B])/1000.0
+        pf2 = np.int16(response.registers[0x3C])/1000.0
+        pf3 = np.int16(response.registers[0x3D])/1000.0
         
-        mqtt_client.publish("smartmeter/pf", payload=pf)
-        mqtt_client.publish("smartmeter/pf1", payload=pf1)
-        mqtt_client.publish("smartmeter/pf2", payload=pf2)
-        mqtt_client.publish("smartmeter/pf3", payload=pf3)
+        if(pf != pf_):
+            mqtt_client.publish("smartmeter/pf", payload=pf)
+        if(pf1 != pf1_):
+            mqtt_client.publish("smartmeter/pf1", payload=pf1)
+        if(pf2 != pf2_):
+            mqtt_client.publish("smartmeter/pf2", payload=pf2)
+        if(pf3 != pf3_):
+            mqtt_client.publish("smartmeter/pf3", payload=pf3)
+            
+        # frequency
+        f = np.int16(response.registers[0x2C])/100.0
+        
+        if(f != f_):
+            mqtt_client.publish("smartmeter/f", payload=f)
     
     response = client.read_holding_registers(address=0x5000, count=0x08, unit=1)
     
     if hasattr(response, "registers"):
         # energy forward
-        ef = (np.left_shift(response.registers[0x00],48) | np.left_shift(response.registers[0x01],32) | np.left_shift(response.registers[0x02],16) | response.registers[0x03]) / 1000.0
-        ee = (np.left_shift(response.registers[0x04],48) | np.left_shift(response.registers[0x05],32) | np.left_shift(response.registers[0x06],16) | response.registers[0x07]) / 1000.0
+        ef = (np.left_shift(response.registers[0x00],48) | np.left_shift(response.registers[0x01],32) | np.left_shift(response.registers[0x02],16) | response.registers[0x03]) / 100.0
+        ee = (np.left_shift(response.registers[0x04],48) | np.left_shift(response.registers[0x05],32) | np.left_shift(response.registers[0x06],16) | response.registers[0x07]) / 100.0
        
-        mqtt_client.publish("smartmeter/ef", payload=ef)
-        mqtt_client.publish("smartmeter/ee", payload=ee)
+        if(ef != ef_):
+            mqtt_client.publish("smartmeter/ef", payload=ef)
+        if(ee != ee_):
+            mqtt_client.publish("smartmeter/ee", payload=ee)
         
     response = client.read_holding_registers(address=0x5460, count=0x18, unit=1)
     if hasattr(response, "registers"):
         
-        ef1 = (np.left_shift(response.registers[0x0],48) | np.left_shift(response.registers[0x01],32) | np.left_shift(response.registers[0x02],16) | response.registers[0x03]) / 1000.0
-        ef2 = (np.left_shift(response.registers[0x04],48) | np.left_shift(response.registers[0x05],32) | np.left_shift(response.registers[0x06],16) | response.registers[0x07]) / 1000.0
-        ef3 = (np.left_shift(response.registers[0x08],48) | np.left_shift(response.registers[0x09],32) | np.left_shift(response.registers[0x0A],16) | response.registers[0x0B]) / 1000.0
+        ef1 = (np.left_shift(response.registers[0x0],48) | np.left_shift(response.registers[0x01],32) | np.left_shift(response.registers[0x02],16) | response.registers[0x03]) / 100.0
+        ef2 = (np.left_shift(response.registers[0x04],48) | np.left_shift(response.registers[0x05],32) | np.left_shift(response.registers[0x06],16) | response.registers[0x07]) / 100.0
+        ef3 = (np.left_shift(response.registers[0x08],48) | np.left_shift(response.registers[0x09],32) | np.left_shift(response.registers[0x0A],16) | response.registers[0x0B]) / 100.0
 
-        mqtt_client.publish("smartmeter/ef1", payload=ef1)
-        mqtt_client.publish("smartmeter/ef2", payload=ef2)
-        mqtt_client.publish("smartmeter/ef3", payload=ef3)
+        if(ef1 != ef1_):
+            mqtt_client.publish("smartmeter/ef1", payload=ef1)
+        if(ef2 != ef2_):
+            mqtt_client.publish("smartmeter/ef2", payload=ef2)
+        if(ef3 != ef3_):
+            mqtt_client.publish("smartmeter/ef3", payload=ef3)
         
-        ee1 = (np.left_shift(response.registers[0x0C],48) | np.left_shift(response.registers[0x0D],32) | np.left_shift(response.registers[0x0E],16) | response.registers[0x0F]) / 1000.0
-        ee2 = (np.left_shift(response.registers[0x10],48) | np.left_shift(response.registers[0x11],32) | np.left_shift(response.registers[0x12],16) | response.registers[0x13]) / 1000.0
-        ee3 = (np.left_shift(response.registers[0x14],48) | np.left_shift(response.registers[0x15],32) | np.left_shift(response.registers[0x16],16) | response.registers[0x17]) / 1000.0
+        ee1 = (np.left_shift(response.registers[0x0C],48) | np.left_shift(response.registers[0x0D],32) | np.left_shift(response.registers[0x0E],16) | response.registers[0x0F]) / 100.0
+        ee2 = (np.left_shift(response.registers[0x10],48) | np.left_shift(response.registers[0x11],32) | np.left_shift(response.registers[0x12],16) | response.registers[0x13]) / 100.0
+        ee3 = (np.left_shift(response.registers[0x14],48) | np.left_shift(response.registers[0x15],32) | np.left_shift(response.registers[0x16],16) | response.registers[0x17]) / 100.0
 
-        mqtt_client.publish("smartmeter/ee1", payload=ee1)
-        mqtt_client.publish("smartmeter/ee2", payload=ee2)
-        mqtt_client.publish("smartmeter/ee3", payload=ee3)
+        if(ee1 != ee1_):
+            mqtt_client.publish("smartmeter/ee1", payload=ee1)
+        if(ee2 != ee2_):
+            mqtt_client.publish("smartmeter/ee2", payload=ee2)
+        if(ee3 != ee3_):
+            mqtt_client.publish("smartmeter/ee3", payload=ee3)
 
 client.close()
